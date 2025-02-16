42f78d95026ecec94cebfc4121fe1a887853d627
Rahul Bhati <rahulbhati0202@gmail.com>
2025-02-16 21:38:05 +0530

    Redux Thunk in React: Fetch and Add API Data to Your To-Do App

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { MdDelete } from "react-icons/md";
+import { addTask, deleteTask, fetchTask } from './store';
 
 const Todo = () => {
-  return (
-    <div>
-      <h3>Todo - App</h3>
-    </div>
-  )
+    const [data, setData] = useState("")
+    const store = useSelector((store) => store.task)
+
+    const dispatch = useDispatch()
+
+    const handleDelete = (id) => {
+        return dispatch(deleteTask(id))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setData("")
+        return dispatch(addTask(data))
+
+    }
+
+    const handleChanged = (event) => {
+        setData(event.target.value)
+    }
+    console.log(data)
+
+    const HandleFetchData=()=>{
+        return dispatch(fetchTask())
+    }
+    return (
+        <div>
+            <div className="container">
+                <div className="todo-app">
+                    <h1>
+                        <i className='fa-regular fa-pen-to-square'></i>To-do List:
+                    </h1>
+                    <div className="row">
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" value={data} onChange={handleChanged} id='input-box' placeholder='Add a new task' />
+                            <button>Add Task</button>
+                        </form>
+                        <button onClick={HandleFetchData}>Fetch All</button>
+                    </div>
+                    <ul id='list-container'>
+                        {store && store.map((element, index) => (
+                            <li key={index}>
+                                <p>{index}: {element}</p>
+                                <div className="icon-style">
+                                    <MdDelete onClick={() => handleDelete(index)} />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Todo
--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,10 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { thunk } from "redux-thunk";
 
 const ADD_TASK = "task/add"
 const DELETE_TASK = "task/delete"
+const FETCH_TASK = "task/fetch"
 
 const initalState = {
     task: [],
@@ -25,7 +28,11 @@ const taskReducer = (state = initalState, action) => {
                 ...state,
                 task: UpdatedTask
             }
-
+        case FETCH_TASK:
+            return {
+                ...state,
+                task: [...state.task, ...action.payload]
+            }
 
         default:
             return state
@@ -33,18 +40,28 @@ const taskReducer = (state = initalState, action) => {
 }
 
 // Create Action creator
-const addTask = (data) => {
+export const addTask = (data) => {
     return { type: ADD_TASK, payload: data }
 }
 
-const deleteTask = (id) => {
+export const deleteTask = (id) => {
     return { type: DELETE_TASK, payload: id }
 }
-export const store = createStore(taskReducer)
+export const store = createStore(taskReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 store.dispatch(addTask("I am a payload 1"))
 store.dispatch(addTask("I am a payload2"))
-store.dispatch(deleteTask(1))
+// store.dispatch(deleteTask(1))
 
 
-console.log(3333, store.getState())
+export const fetchTask = () => {
+    return async (dispatch) => {
+        try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=3")
+            const newData = await res.json()
+            dispatch({ type: FETCH_TASK, payload: newData.map((elememt) => elememt.title) })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
